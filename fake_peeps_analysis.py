1b63c9a77ca9fa3949d0ba9e899085c067766208
Annie Kramer <anniekramer@dropbox.com>
2014-07-15 13:08:49 -0700

    Compute breakdown of genders in test group

--- fake_peeps_analysis.py
+++ fake_peeps_analysis.py
@@ -1,4 +1,5 @@
 import json
+from collections import Counter
 
 data_string = open('fake_peeps.json', 'r').read()
 print data_string
@@ -10,20 +11,30 @@ data_encoded = json.dumps(data_decoded)
 print data_encoded
 
 print data_decoded
+
 # use data_decoded to find average age of folks
 # in test json file
-
 age_list = [dictionary["age"] for dictionary in data_decoded]
 average_age = sum(age_list) / len(age_list)
-print average_age
+# print average_age
 print "The average age of fake folks is " + str(average_age) + "."
 
-female_count = 0
-male_count = 0
+# calculate total number of people in json test file
+count = 0
 for dictionary["gender"] in data_decoded:
-    if [dictionary["gender"]] == "female":
-        female_count = female_count + 1
-    else:
-        male_count = male_count + 1
-print female_count
-print male_counts
+    count = count + 1
+# print count
+print "There are " + str(count) + " people in this motley crew."
+
+# calculate totals for women and men in json test file
+gender_list = [dictionary["gender"] for dictionary in data_decoded]
+print gender_list
+
+num_females = gender_list.count("female")
+print num_females
+
+num_males = gender_list.count("male")
+print num_males
+
+
+
